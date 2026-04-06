126b80b83fa1bc1b750056462e62145a190f4a5f
RPiGPIO <brownluckkapankha@gmail.com>
2026-04-07 02:36:47 +0530

    Update csv.py

--- csv.py
+++ csv.py
@@ -1,51 +1,25 @@
+import pandas as pd
 import requests
 import xml.etree.ElementTree as ET
-import csv
 
-def load_rss(url, xml_file):
-    response = requests.get(url)
-    with open(xml_file, "wb") as f:
-        f.write(response.content)
-    print("XML file saved successfully")
+url = "https://feeds.feedburner.com/50WordStories"
+resp = requests.get(url)
 
-def parse_xml(xml_file):
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
+root = ET.fromstring(resp.content)
 
-    news_items = []
+items = []
 
-    for item in root.findall(".//item"):
-        news = {
-            "title": item.findtext("title"),
-            "link": item.findtext("link"),
-            "pubDate": item.findtext("pubDate"),
-            "description": item.findtext("description")
-        }
-        news_items.append(news)
+for item in root.findall(".//item"):
+    data = {}
+    for child in item:
+        tag = child.tag.split("}")[-1]
 
-    return news_items
+        if tag in ["guid", "title", "pubDate", "description", "link"]:
+            data[tag] = child.text
 
-def save_to_csv(news_items, csv_file):
-    fields = ["title", "link", "pubDate", "description"]
+    items.append(data)
 
-    with open(csv_file, "w", newline="", encoding="utf-8") as f:
-        writer = csv.DictWriter(f, fieldnames=fields)
-        writer.writeheader()
-        writer.writerows(news_items)
+df = pd.DataFrame(items)
+df.to_csv("topnews.csv", index=False)
 
-    print("CSV file created successfully")
-
-def main():
-    rss_url = "https://feeds.feedburner.com/50WordStories"
-
-    xml_file = "news.xml"
-    csv_file = "news.csv"
-
-    load_rss(rss_url, xml_file)
-
-    items = parse_xml(xml_file)
-
-    save_to_csv(items, csv_file)
-
-if __name__ == "__main__":
-    main()
+print("Saved to topnews.csv")
